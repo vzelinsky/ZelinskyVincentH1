471ebacb4a6099a49e6a052bfdb3b81ba6d68131
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-09-11 14:25:36 -0400

    Initial commit, stating purpose and creating an empty main method.

---
+++ ZelinskyVincentH1.java
@@ -0,0 +1,11 @@
+/* ZelinskyVincentH1.java
+ * Written by Vincent Zelinsky
+ * 
+ * Demonstrates Object Oriented Design principles, class inheritance and data encapsulation, through classes representative of things you'd find in a Role Playing Game.
+ */
+class ZelinskyVincentH1 
+{
+  public static void main (String[] args)
+  {
+  }
+}
