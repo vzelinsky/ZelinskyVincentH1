e078e3e14a24fe161f15318ad1537c789deb9085
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-09-16 01:20:56 -0400

    Adds to the output of the Note_1 object, creates a Weapon object and initializes its values, adds print statements to validate

--- GameMain.java
+++ GameMain.java
@@ -7,7 +7,14 @@
 public class GameMain{
   //Main method that creates objects, tests their methods, and prints results to console for validation
   public static void main (String[] args){
+    //Creates an Item object called Note_1 and passes values to its fields
     Item Note_1 = new Item ("Random note",0,"A note that reads:\n\"To whom it may concern,\nIts dangerous to go alone take these items.\"");
-    System.out.println (Note_1.getExamine());
+    //Prints the returned values from Note_1 for validation
+    System.out.println (Note_1.getName()+"\n"+Note_1.getWeight()+"\n"+Note_1.getExamine()+"\n");
+    
+    //Creates a Weapon object called Sword_1 and passes values to its fields
+    Weapon Sword_1 = new Weapon ("Steel sword",3,"An average longsword not remarkable in any way.",30);
+    //Prints the returned values from Sword_1 for validation
+    System.out.println (Sword_1.getName()+"\n"+Sword_1.getWeight()+"\n"+Sword_1.getDamage()+"\n"+Sword_1.getExamine()+"\n");
   }
 }
