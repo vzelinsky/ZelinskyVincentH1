c6c55a43670a0661e2e5987f94506fde4f040ed1
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-09-17 22:56:47 -0400

    Created a Food object called Food_1 which tests its get methods

--- GameMain.java
+++ GameMain.java
@@ -22,5 +22,10 @@ public class GameMain{
     Armor Armor_1 = new Armor ("Leather Armor",15,"Hardened leather plates, normally used for sparring, this armor can at least dampen any blows received.",2);
     //Prints the returned values from Sword_1 for validation
     System.out.println (Armor_1.getName()+"\n"+Armor_1.getWeight()+"\n"+Armor_1.getDefense()+"\n"+Armor_1.getExamine()+"\n");
+    
+    //Creates a Food object called Food_1 and passes values to its fields
+    Food Food_1 = new Food ("Bread",1,"One fresh loaf of bread.",30,1);
+    //Prints the returned values from Food_1 for validation
+    System.out.println (Food_1.getName()+"\n"+Food_1.getWeight()+"\n"+Food_1.getNutrition()+"\n"+Food_1.getQuantity()+"\n"+Food_1.getExamine()+"\n");
   }
 }
