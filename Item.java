f891250026238e16fe1305f5c41b67098b743044
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-09-17 22:02:31 -0400

    Fixes the wall I've been hitting and adds a second constructor to Item so my Weapon constructor doesn't have to use Item's examine field. Then Overloades the getExamine method so I can add the damage field to its return.

--- Item.java
+++ Item.java
@@ -1,3 +1,9 @@
+/* ~9:45 9/17/16: 
+ * I cannot find a reason to allow other team members to use setters on name, examine, and especially the weight fields.
+ * These fields, once set in an object's creation, have no reason to be set again. Why would you change the name? Why would you change the weight?
+ * The only reason I can see these needing to be set is if the object was somehow modified by the player and instead of using a new object you wanted to edit this current one.
+ * I will add them for the sake of convenience for my pretend team if I have extra time.
+ */
 //Creates the superclass Item which contains the fields for name, weight, and examine
 public class Item {
   //Declares three fields: name, weight, and examine
@@ -5,11 +11,16 @@ public class Item {
   private int weight;
   private String examine;
   
-  //Creates a constructor for Item
+  //Creates a constructor for Item that is meant to be invoked by the creation of an Item object
   public Item (String name, int weight, String examine){
     this.name = name;
     this.weight = weight;
     this.examine = examine;
+  }
+  //Adds a second constructor for Item that is meant to be invoked by any subclass constructors
+  public Item (String name, int weight){
+    this.name = name;
+    this.weight = weight;
   }
   //Creates method to get name
   public String getName(){
--- Weapon.java
+++ Weapon.java
@@ -2,14 +2,24 @@
 public class Weapon extends Item{
   //Declares the field damage
   private int damage;
+  private String examine;
   
-  //Creates a constructor for Weapon
+  //Creates a constructor for Weapon that invokes the second constructor in Item and adds to it
   public Weapon (String name, int weight, String examine, int damage){
-    super(name, weight,examine);
+    super(name, weight);
+    this.examine = examine;
     this.damage = damage;
   }
   //Creates a method to get damage
   public int getDamage(){
     return damage;
+  }
+  //Overloads the getExamine method so when called it adds damage
+  /* It was here that I decided that the player can only interact with the various objects of the game through the getExamine method.
+   * Therefore, the getExamine method needs to give the player all the information they could require.
+   */ 
+  @Override
+  public String getExamine(){
+    return examine+"\nIts name is: "+getName()+"\nIts weight is: "+getWeight()+"\nIts damage is: "+damage;
   }
 }
